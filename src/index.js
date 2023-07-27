ca28568b597f3dcf1561cc80adcf87bba75bfb76
OlegKofman <oleg.kofman@run.ai>
2023-07-27 09:06:03 +0300

    indexjs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Header() {
-  return <h1>stonkbot</h1>;
-}
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Stonkbot</h1>
+      </header>
+    );
+  }
+} 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <p>hello world</p>
-    </div>
-  );
-}
-
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Stonkbot</h1>
+      </div>
+    );
+  }
+} 
 ReactDOM.render(<App />, document.getElementById("root"));
