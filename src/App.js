8cfb4ea19d1f3b0d052965bda437c49d71be9c02
OlegKofman <oleg.kofman@run.ai>
2023-07-27 13:11:17 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,32 +3,28 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import SearchLabel from "./components/SearchLabel";
 import Home from "./components/Home";
-
 import "./css/App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header>
-            <nav>
-              <Link to="/">Home</Link>
-              <Link to="/stock-search">Stock Analyzer</Link>
-              {/* Add more menu items as needed */}
-            </nav>
-          </header>
-          <div className="container">
-            <Header /> {/* Render the Header component here */}
-            <Routes>
-              <Route path="/stock-search" element={<SearchLabel />} />
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </div>
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/stock-search">Stock Analyzer</Link>
+          </nav>
+        </header>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/stock-search" element={<SearchLabel />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
-      </Router>
-    );
-  }
-}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
