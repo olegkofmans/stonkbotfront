c44c931c644465d45f05d3d91e23ee60e9b6fb6d
OlegKofman <oleg.kofman@run.ai>
2023-07-27 13:39:13 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Menu from "./components/Menu";
 import SearchLabel from "./components/SearchLabel";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 import "./css/App.css";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <header>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/stock-search">Stock Analyzer</Link>
-          </nav>
-        </header>
+        <Header />
+        <Menu />
         <div className="container">
-          <Header />
           <Routes>
             <Route path="/stock-search" element={<SearchLabel />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
+        <Footer />
       </div>
     </Router>
   );
